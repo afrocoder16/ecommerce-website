e7e65eb8e0620125fad3c7256d5a797f43334854
Afrocoder <samson.nigusse7@gmail.com>
2024-08-08 07:56:06 -0500

    Update settings.py

--- southwest/settings.py
+++ southwest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8tman@u=rd=y^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['southwest-store.onrender.com']
 
 
 # Application definition
